95d7549b70c0bac0d0fff52da61841624f3b5a2b
Nick Dickinson-Wilde <nick@nickdickinsonwilde.ca>
2022-03-01 16:16:17 -0800

    Add update loop for cases that need to update after initial load - ie ajax

--- js/mapbox.js
+++ js/mapbox.js
@@ -27,10 +27,19 @@
       $.each(repeatMap.load, function(id, fn){
         fn(map);
       });
+      this.update();
+    }
+
+    update() {
+      const map = this;
+      $.each(repeatMap.update, function(id, fn){
+        fn(map);
+      });
     }
   }
   window.repeatMap = {
     instances: [],
+    update: {},
     load: {
       addNavigation: function(map) {
         map.controls.nav = new mapboxgl.NavigationControl();
@@ -44,33 +53,11 @@
       $('.repeat_mapbox').once().each(function() {
         repeatMap.instances.push(new RepeatMap(this));
       });
-
-      if (settings.repeat_mapbox && settings.repeat_mapbox.geojson && Drupal.repeat_map.map) {
-        if (Drupal.repeat_map.hash !== settings.repeat_mapbox.hash) {
-          if (Drupal.repeat_map.markers !== undefined) {
-            for (var i = Drupal.repeat_map.markers.length - 1; i >= 0; i--) {
-              Drupal.repeat_map.markers[i].remove();
-            }
-          }
-          Drupal.repeat_map.markers = [];
-          settings.repeat_mapbox.geojson.features.forEach(function (marker) {
-            var el = document.createElement('div');
-            el.className = 'mapbox-marker';
-            if (marker.properties.field_pathway) {
-              el.className += ' pathway--' + marker.properties.field_pathway.toLowerCase();
-            }
-            // Make a marker for each feature and add to the map.
-            var status_with_label = (marker.properties.field_status) ? 'Status: ' + marker.properties.field_status : '';
-            var temp_marker = new mapboxgl.Marker(el)
-                .setLngLat(marker.geometry.coordinates)
-                .setPopup(new mapboxgl.Popup({offset: 25})
-                    .setHTML('<span class="status">' + status_with_label + '</span><div class="properties"><h3>' + marker.properties.name + '</h3><p>' + marker.properties.description + '</p></div>'))
-                .addTo(Drupal.repeat_map.map);
-            Drupal.repeat_map.markers.push(temp_marker);
-          });
-          Drupal.repeat_map.hash = settings.repeat_mapbox.hash;
-          drupalSettings.repeat_mapbox.geojson = settings.repeat_mapbox.geojson = null;
-        }
+      if (settings.repeat_mapbox && settings.repeat_mapbox.update) {
+        $.each(repeatMap.instances, function(n, map){
+          map.update();
+        });
+        settings.repeat_mapbox.update = false;
       }
     }
   };
